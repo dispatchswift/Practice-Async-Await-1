26f56b895de80301756fb19c2b99a17d91359697
Amari Duran <jch.duran1@gmail.com>
2022-02-19 21:48:49 -0700

    Practice with Async/Await

---
+++ LittleJohn/Extensions/String+.swift
@@ -0,0 +1,11 @@
+//
+//  String+.swift
+//  LittleJohn
+//
+//  Created by DispatchSwift on 2/19/22.
+//
+
+import Foundation
+
+/// Easily throw generic errors with a text description.
+extension String: Error { }
---
+++ LittleJohn/Models/Stock.swift
@@ -0,0 +1,13 @@
+//
+//  Stock.swift
+//  LittleJohn
+//
+//  Created by DispatchSwift on 2/19/22. 
+//
+
+import Foundation
+
+struct Stock: Hashable, Codable {
+	let name: String
+	let value: Double
+}
